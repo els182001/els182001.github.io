4f0eaa82f4e15e857496fb86f889169cbce11973
Elliot Stern <elst4279@colorado.edu>
2023-04-04 09:34:39 -0600

    Update main10.js

--- ICA/WA10/main10.js
+++ ICA/WA10/main10.js
@@ -5,9 +5,15 @@ const btn = document.querySelector('button');
 const overlay = document.querySelector('.overlay');
 
 /* Declaring the array of image filenames */
-
+const Array = ['pic1.jpg', 'pic2.jpg','pic3.jpg', 'pic4.jpg', 'pic5.jpg']
 /* Declaring the alternative text for each image file */
-
+const alt = {
+    'pic1.jpg' : 'Eye',
+    'pic2.jpg' : 'gray',
+    'pic3.jpg' : 'flowers',
+    'pic4.jpg' : 'egypt',
+    'pic5.jpg' : 'butterfly'
+};
 /* Looping through images */
 
 const newImage = document.createElement('img');
